676593fbf6dacbd394e974749d67da4394b7ce92
sumithraK <sumithra.cs13@bitsathy.ac.in>
2016-07-22 23:31:08 +0530

    Update README.c

--- README.c
+++ README.c
@@ -6,7 +6,7 @@ int main(void)
 
  char a[5][5]={{'w','e','l','c','o'},{'m','e','t','o','o'},{'i','t','o','a','t'},{'h','y','o','m','a'},{'n','g','a','l','a'}};
   
-int i,j,count=0;
+int i,j;
    
  for(i=0;i<5;i++)
   
@@ -19,7 +19,7 @@ int i,j,count=0;
    if((a[i][j]=='t')&&(a[i][j+1]=='o')&&(a[i][j+2]=='o'))
 
              {
-                 count++;
+                 printf("Start Index:<%d %d>End Index<%d %d>",i,j,i,j+2)
              }
    
       }
@@ -36,13 +36,13 @@ for(i=0;i<5;i++)
          if((a[i][j]=='t')&&(a[i+1][j]=='o')&&(a[i+2][j]=='o'))
     
          {
-                 count++;
+               printf("Start Index:<%d %d>End Index<%d %d>",i,j,i,j+2)
 
              }
          }
     }
 
-    printf("%d count",count);
+    
 	// your code goes here
 	return 0;
 
