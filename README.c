449608c2a382a61d36609294e3f368c05ff3d8a1
sumithraK <sumithra.cs13@bitsathy.ac.in>
2016-07-22 23:21:11 +0530

    Update and rename README. to README.c

---
+++ README.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+
+int main(void) 
+{
+
+ char a[5][5]={{'w','e','l','c','o'},{'m','e','t','o','o'},{'i','t','o','a','t'},{'h','y','o','m','a'},{'n','g','a','l','a'}};
+  
+int i,j,count=0;
+   
+ for(i=0;i<5;i++)
+  
+  {
+ 
+        for(j=0;j<5;j++)
+
+         {
+          
+   if((a[i][j]=='t')&&(a[i][j+1]=='o')&&(a[i][j+2]=='o'))
+
+             {
+                 count++;
+             }
+   
+      }
+    }
+     
+for(i=0;i<5;i++)
+ 
+   {
+      
+   for(j=0;j<5;j++)
+ 
+        {
+    
+         if((a[i][j]=='t')&&(a[i+1][j]=='o')&&(a[i+2][j]=='o'))
+    
+         {
+                 count++;
+
+             }
+         }
+    }
+
+    printf("%d count",count);
+	// your code goes here
+	return 0;
+
+}
+
